25e0bb1bc3908e44915a23da0c899b9d0bb13977
fedcorpweb <fedcorpweb@gmail.com>
2020-10-04 18:39:02 -0300

    update spring boot version / add profiles

--- base-project-api/README.md
+++ base-project-api/README.md
@@ -1,7 +1,7 @@
 # BASE-PROJECT-API
  
-### compile --> mvn clean package -Dmaven.test.skip=true
-### run --> java -jar target/base-project-api.jar
+### compile --> mvn clean package -Dmaven.test.skip=true -P "profile-id"
+### run --> mvn spring-boot:run -P "profile-id"
 ### app config --> resources/application.properties
 ### db console --> http://localhost:8080/h2-console
 
--- base-project-api/src/main/java/com/baseproject/BaseProjectApplication.java
+++ base-project-api/src/main/java/com/baseproject/BaseProjectApplication.java
@@ -5,10 +5,13 @@ import java.util.concurrent.Executor;
 
 import javax.annotation.PostConstruct;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
@@ -17,13 +20,18 @@ import com.baseproject.services.StartService;
 @SpringBootApplication
 @EnableAsync
 public class BaseProjectApplication {
-	
+
+	protected final Log log = LogFactory.getLog(BaseProjectApplication.class);
+
+	@Autowired
+	Environment env;
+
 	@Autowired
 	StartService startService;
 
-
 	@PostConstruct
 	void init() {
+		log.info("##### PROFILE: " + env.getProperty("profile") + " #####");
 		TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
 		startService.init();
 	}
@@ -31,15 +39,15 @@ public class BaseProjectApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BaseProjectApplication.class, args);
 	}
-	
+
 	@Bean
-    public Executor taskExecutor() {
-        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
-        executor.setCorePoolSize(2);
-        executor.setMaxPoolSize(2);
-        executor.setQueueCapacity(500);
-        executor.initialize();
-        return executor;
-    }
+	public Executor taskExecutor() {
+		ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+		executor.setCorePoolSize(2);
+		executor.setMaxPoolSize(2);
+		executor.setQueueCapacity(500);
+		executor.initialize();
+		return executor;
+	}
 
 }
